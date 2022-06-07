b76c2726d2f19dde884ae8b2f88926c0ec9675ea
Nehal <nehal.naeem2@gmail.com>
2022-06-07 16:26:09 -0700

    part1 setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const Hello = (props) => {
+  console.log('Hello from component -> Hello')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Hello {props.name}, you are {props.age} years old</p>
     </div>
-  );
+  )
+}
+const App = () => {
+  console.log('Hello from component -> App')
+  const name = "Peter";
+  const age = 10;
+
+  return (
+    <div>
+      <h1>Greetings!</h1>
+      <Hello name="George" age={26 + 10} />
+      <Hello name={name} age={age}/>
+    </div>
+  )
 }
 
 export default App;
