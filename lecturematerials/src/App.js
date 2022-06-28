5543a9bf5e098dce748cbe88de75e92b46602c42
Nehal <nehal.naeem2@gmail.com>
2022-06-27 17:30:41 -0700

    finished module for part1 and setup for unicafe

--- lecturematerials/src/App.js
+++ lecturematerials/src/App.js
@@ -1,32 +1,51 @@
 import { useState } from "react";
 
-function Display(props) {
-  return (
-    <h1>{props.counter}</h1>
-  );
-}
-
 function Button(props) {
+  console.log("props value is ", props)
+  const {handleClick, text} = props
   return (
-    <button onClick={props.onClick}>
-      {props.text}
+    <button onClick={handleClick}>
+      {text}
     </button>
   )
 }
 
+function History({allClicks}) {
+  if (allClicks.length === 0) {
+    return (
+      <div>
+        The app is used by pressing the buttons
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        Button press history: {allClicks.join(' ')}
+      </div>
+    )
+  }
+}
 function App() {
-  const [counter, setCounter] = useState(0);
-
-  function incrementCounter() {setCounter(counter + 1)}
-  function decrementCounter() {setCounter(counter - 1)}
-  function resetCounter() {setCounter(0)}
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
+  
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'));
+    setLeft(left + 1);
+  }
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'));
+    setRight(right + 1);
+  }
 
   return (
     <div>
-      <Display counter={counter}/>
-      <Button onClick={incrementCounter} text="Add to Counter" />
-      <Button onClick={resetCounter} text="Reset Counter" />
-      <Button onClick={decrementCounter} text="Subtract from Counter" />
+      {left}
+      <Button handleClick={handleLeftClick} text='Left' />
+      <Button handleClick={handleRightClick} text='Right' />
+      {right}
+      <History allClicks={allClicks} />
     </div>
   );
 }
